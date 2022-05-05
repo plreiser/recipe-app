b6f1634ee31828f0c8aa5b702de20ab05dc74be2
Pablo Reiser <prkubli@gmail.com>
2022-05-05 20:53:25 +0800

    Add RecipeDetailView and NavigationLink

--- Recipe List App/Views/RecipeListView.swift
+++ Recipe List App/Views/RecipeListView.swift
@@ -13,18 +13,21 @@ struct RecipeListView: View {
     @ObservedObject var model = RecipeModel()
     
     var body: some View {
-        
-        List(model.recipes) { r in
-            
-            HStack(spacing: 20.0) {
-                Image(r.image)
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 50, height: 50, alignment: .center)
-                    .clipped()
-                    .cornerRadius(5)
-                Text(r.name)
+        NavigationView {
+            List(model.recipes) { r in
+                NavigationLink (destination: RecipeDetailView(recipe:r), label: {
+                    HStack(spacing: 20.0) {
+                        Image(r.image)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 50, height: 50, alignment: .center)
+                            .clipped()
+                            .cornerRadius(5)
+                        Text(r.name)
+                    }
+                })
             }
+            .navigationTitle("All Recipes")
         }
     }
 }
