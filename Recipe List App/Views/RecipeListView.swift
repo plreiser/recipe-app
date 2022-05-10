f39248b855666787c6009e4a9548d93fa0116eaa
Pablo Reiser <prkubli@gmail.com>
2022-05-10 20:26:12 +0800

    - build UI in the FeaturedView

--- Recipe List App/Views/RecipeListView.swift
+++ Recipe List App/Views/RecipeListView.swift
@@ -34,5 +34,6 @@ struct RecipeListView: View {
 struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
+            .environmentObject(RecipeModel())
     }
 }
