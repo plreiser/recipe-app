e5b3b0596baef2809615c294d423eb14ebe50878
Pablo Reiser <prkubli@gmail.com>
2022-05-16 19:01:38 +0800

    - change swiftUI list to scroll view, lazyVStack and ForEach -Hide navigation bar title

--- Recipe List App/Views/RecipeListView.swift
+++ Recipe List App/Views/RecipeListView.swift
@@ -13,20 +13,35 @@ struct RecipeListView: View {
     
     var body: some View {
         NavigationView {
-            List(model.recipes) { r in
-                NavigationLink (destination: RecipeDetailView(recipe:r), label: {
-                    HStack(spacing: 20.0) {
-                        Image(r.image)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 50, height: 50, alignment: .center)
-                            .clipped()
-                            .cornerRadius(5)
-                        Text(r.name)
+            
+            VStack(alignment: .leading) {
+                Text("All Recipes")
+                    .bold()
+                    
+                    .padding(.top, 40)
+                    .font(.largeTitle)
+                
+                ScrollView {
+                    LazyVStack(alignment: .leading) {
+                        ForEach(model.recipes) { r in
+                            NavigationLink (destination: RecipeDetailView(recipe:r), label: {
+                                HStack(spacing: 20.0) {
+                                    Image(r.image)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 50, height: 50, alignment: .center)
+                                        .clipped()
+                                        .cornerRadius(5)
+                                    Text(r.name)
+                                        .foregroundColor(.black)
+                                }
+                            })
+                        }
                     }
-                })
+                }
             }
-            .navigationTitle("All Recipes")
+            .navigationBarHidden(true)
+            .padding(.leading)
         }
     }
 }
